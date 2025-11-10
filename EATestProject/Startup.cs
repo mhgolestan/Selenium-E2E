35200da450b2005e12455fe234ae0587eb1a6205
mhgolestan <golestan1369@gmail.com>
2025-11-10 01:58:17 +0100

    Add appsettings.json for browser configuration and update Startup to use it

--- EATestProject/Startup.cs
+++ EATestProject/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using EATestFramework.Driver;
 using EATestProject.Pages;
 using EATestFramework.Extensions;
+using Microsoft.Extensions.Configuration;
 
 namespace EATestProject;
 
@@ -9,7 +10,9 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        services.UseWebDriverInitializer(BrowserType.Chrome);
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var browserType = config.GetSection("BrowserType").Value; 
+        services.UseWebDriverInitializer((BrowserType)(Enum.Parse(typeof(BrowserType), browserType, ignoreCase: true)));
         services.AddScoped<IDriverFixture, DriverFixture>();
         services.AddScoped<IBrowserDriver, BrowserDriver>();
         services.AddScoped<IHomePage, HomePage>();
