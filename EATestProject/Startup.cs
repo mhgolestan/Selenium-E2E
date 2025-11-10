22dc53f031194366d7e4da2eaa35d095985c4dc1
mhgolestan <golestan1369@gmail.com>
2025-11-10 10:21:39 +0100

    refactor browsertype conf reading

--- EATestProject/Startup.cs
+++ EATestProject/Startup.cs
@@ -11,8 +11,12 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-        var browserType = config.GetSection("BrowserType").Value; 
-        services.UseWebDriverInitializer((BrowserType)(Enum.Parse(typeof(BrowserType), browserType, ignoreCase: true)));
+        var browserTypeString = config.GetSection("BrowserType").Value;
+        if (!Enum.TryParse<BrowserType>(browserTypeString, true, out var browserType))
+        {
+            throw new ArgumentException($"BrowserType '{browserTypeString}' in appsettings.json is not a supported browser.");
+        }
+        services.UseWebDriverInitializer(browserType);
         services.AddScoped<IDriverFixture, DriverFixture>();
         services.AddScoped<IBrowserDriver, BrowserDriver>();
         services.AddScoped<IHomePage, HomePage>();
