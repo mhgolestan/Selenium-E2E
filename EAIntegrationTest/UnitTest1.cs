24385f4a062b3c719570c104f2828632cf0d3a75
mhgolestan <golestan1369@gmail.com>
2025-11-13 10:58:15 +0100

    Add initial unit test for product retrieval using HttpClient

--- EAIntegrationTest/UnitTest1.cs
+++ EAIntegrationTest/UnitTest1.cs
@@ -5,6 +5,9 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:8001/");
+        var response = client.Send(new HttpRequestMessage(HttpMethod.Get, "Product/GetProducts"));
+        response.EnsureSuccessStatusCode();
     }
 }
