c9aadcabe2053618dd369dc36061d63b2c8bb7c5
mhgolestan <golestan1369@gmail.com>
2025-11-13 13:05:10 +0100

    Update .gitignore, enhance unit tests with FluentAssertions and WebApplicationFactory, and modify connection string in appsettings.json

--- EAIntegrationTest/UnitTest1.cs
+++ EAIntegrationTest/UnitTest1.cs
@@ -1,7 +1,18 @@
-﻿namespace EAIntegrationTest;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using ProductAPI;
 
-public class UnitTest1
+namespace EAIntegrationTest;
+
+public class UnitTest1 : IClassFixture<WebApplicationFactory<ProductAPI.Startup>>
 {
+    private readonly WebApplicationFactory<Startup> webApplicationFactory;
+
+    public UnitTest1(WebApplicationFactory<ProductAPI.Startup> webApplicationFactory)
+    {
+        this.webApplicationFactory = webApplicationFactory;
+    }
+
     [Fact]
     public void Test1()
     {
@@ -9,5 +20,17 @@ public class UnitTest1
         client.BaseAddress = new Uri("http://localhost:8001/");
         var response = client.Send(new HttpRequestMessage(HttpMethod.Get, "Product/GetProducts"));
         response.EnsureSuccessStatusCode();
+    }
+
+    [Fact]
+    public async Task TestWithWebAppFactory()
+    {
+        var webClient = webApplicationFactory.CreateClient();
+
+        var product = await webClient.GetAsync("Product/GetProducts");
+        var result = product.Content.ReadAsStringAsync().Result;
+        result.Should().Contain("Keyboard");
+
+
     }
 }
